28defa7ee1cd97a6f7adccb0a4175c3cabce0360
PierreSQS <nomo_messina@web.de>
2025-12-02 22:49:57 +0100

    Sec7_Chap56-NOK: implemented the Weather Request and the Ninja's WeatherService

--- src/main/java/guru/springframework/springaifunctions/functions/WeatherServiceFunction.java
+++ src/main/java/guru/springframework/springaifunctions/functions/WeatherServiceFunction.java
@@ -2,13 +2,15 @@ package guru.springframework.springaifunctions.functions;
 
 import guru.springframework.springaifunctions.model.WeatherRequest;
 import guru.springframework.springaifunctions.model.WeatherResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestClient;
 
 import java.util.function.Function;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot, 02-12-2025.
  */
+@Slf4j
 public class WeatherServiceFunction implements Function<WeatherRequest, WeatherResponse> {
 
     public static final String WEATHER_URL = "https://api.api-ninjas.com/v1/weather";
@@ -30,16 +32,11 @@ public class WeatherServiceFunction implements Function<WeatherRequest, WeatherR
                 }).build();
 
         return restClient.get().uri(uriBuilder -> {
-            System.out.println("Building URI for weather request: " + weatherRequest);
+            log.info("### Building URI for weather request: {} ###", weatherRequest);
 
-            uriBuilder.queryParam("city", weatherRequest.location());
+            uriBuilder.queryParam("longitude", weatherRequest.lon());
+            uriBuilder.queryParam("latitude", weatherRequest.lat());
 
-            if (weatherRequest.state() != null && !weatherRequest.state().isBlank()) {
-                uriBuilder.queryParam("state", weatherRequest.state());
-            }
-            if (weatherRequest.country() != null && !weatherRequest.country().isBlank()) {
-                uriBuilder.queryParam("country", weatherRequest.country());
-            }
             return uriBuilder.build();
         }).retrieve().body(WeatherResponse.class);
     }
--- src/main/java/guru/springframework/springaifunctions/model/WeatherRequest.java
+++ src/main/java/guru/springframework/springaifunctions/model/WeatherRequest.java
@@ -6,12 +6,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot, 02-12-2025.
+ * <p>
+ * As per Ninja's Weather API, now using lon and lat for location
+ * for the free pricing plan.
+ * <p>
+ * <a href="https://api-ninjas.com/api/weather">ninja documentation</a>
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonClassDescription("Weather API request")
-public record WeatherRequest(@JsonProperty(required = true,
-        value = "location") @JsonPropertyDescription("The city and state e.g. San Francisco, CA") String location,
-        @JsonProperty(required = false) @JsonPropertyDescription("Optional State for US Cities Only. Use full name of State") String state,
-       @JsonProperty(required = false) @JsonPropertyDescription("Optional Country name") String country ){
+@JsonClassDescription("Ninja's Weather API request")
+public record WeatherRequest(@JsonProperty(required = true, value = "lon") @JsonPropertyDescription("Longitude of the city") String lon,
+                             @JsonProperty(required = true, value = "lat") @JsonPropertyDescription("Latitude of the city") String lat){
 }
