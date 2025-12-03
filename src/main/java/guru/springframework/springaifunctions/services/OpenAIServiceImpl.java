a70e40ac89f84d660299d7751561ce0a7cfc5a45
PierreSQS <nomo_messina@web.de>
2025-12-03 10:53:51 +0100

    Sec7_Chap57-NOK: implemented the Function Callback

--- src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
@@ -1,9 +1,16 @@
 package guru.springframework.springaifunctions.services;
 
 
+import guru.springframework.springaifunctions.functions.WeatherServiceFunction;
 import guru.springframework.springaifunctions.model.Answer;
 import guru.springframework.springaifunctions.model.Question;
+import guru.springframework.springaifunctions.model.WeatherRequest;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.model.tool.ToolCallingChatOptions;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.function.FunctionToolCallback;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,14 +19,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    private final ChatClient chatClient;
+
+    @Value("${API_NINJAS_KEY}")
+    private String apiNinjasKey;
+
+    private final ChatClient.Builder chatClientBuilder;
 
     public OpenAIServiceImpl(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClientBuilder = chatClientBuilder;
     }
 
     @Override
     public Answer getAnswer(Question question) {
-       return new Answer("to-do implement me!");
+        // Define the tool callback for the weather service function
+        ToolCallback toolCallback = FunctionToolCallback
+                .builder("currentWeather", new WeatherServiceFunction(apiNinjasKey))
+                .description("Get the weather in location with longitude and latitude")
+                .inputType(WeatherRequest.class)
+                .build();
+
+        // Create chat options with the tool callback
+        ChatOptions chatOptions = ToolCallingChatOptions.builder()
+                .toolCallbacks(toolCallback)
+                .build();
+
+        // Call the chat client with the question and chat options
+        // and return the answer in the JSON format
+        return chatClientBuilder.build()
+                .prompt()
+                .user(question.question())
+                .options(chatOptions)
+                .call()
+                .entity(Answer.class); // format the response in Answer JSON object
+
     }
 }
