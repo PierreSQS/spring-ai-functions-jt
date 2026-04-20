53ff98ff27209112aa6d8866956c12d77b966d1f
PierreSQS <nomo_messina@web.de>
2026-04-20 15:06:03 +0200

    Sec7_Chap57: configured Function CallBack

--- src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
@@ -1,23 +1,45 @@
 package guru.springframework.springaifunctions.services;
 
 
+import guru.springframework.springaifunctions.functions.WeatherServiceFunction;
 import guru.springframework.springaifunctions.model.Answer;
 import guru.springframework.springaifunctions.model.Question;
-import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.model.ChatModel;
+import guru.springframework.springaifunctions.model.WeatherRequest;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.function.FunctionToolCallback;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
-@RequiredArgsConstructor
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    final ChatModel chatModel;
+    private final ChatClient chatClient;
+    private final String apiNinjasKey;
+
+    public OpenAIServiceImpl(ChatClient.Builder chatClientBuilder,
+                             @Value("${sfg.aiapp.apiNinjasKey}") String apiNinjasKey) {
+        this.chatClient = chatClientBuilder.build();
+        this.apiNinjasKey = apiNinjasKey;
+    }
 
     @Override
     public Answer getAnswer(Question question) {
-       return new Answer("to-do implement me!");
+        var weatherFunction = new WeatherServiceFunction(apiNinjasKey);
+
+        var weatherToolCallback = FunctionToolCallback.builder("weatherFunction", weatherFunction)
+                .description("Get current weather for a given longitude and latitude")
+                .inputType(WeatherRequest.class)
+                .build();
+
+        String response = chatClient.prompt()
+                .user(question.question())
+                .tools(weatherToolCallback)
+                .call()
+                .content();
+
+        return new Answer(response);
     }
 }
